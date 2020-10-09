46442f4660404e441665e58c4523f9ac279d4f2d
Senendra <senendra.007@gmail.com>
2020-10-10 01:34:13 +0530

    [Senendra] Add: Creating Mood Analyser Program

---
+++ MyMoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace MyMoodAnalyser
+{
+    class MoodAnalyser
+    {
+        private string message;
+        public string analyseMood(string message)
+        {
+            if (message.Contains("sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        } 
+    }
+}
---
+++ MyMoodAnalyser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MyMoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter message: ");
+            string message = Console.ReadLine();
+            MoodAnalyser checkmood = new MoodAnalyser();
+            string result = checkmood.analyseMood(message);
+            Console.WriteLine(result+" Mood");
+        }
+    }
+}
